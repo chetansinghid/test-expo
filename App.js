6a15d37242f91e1894e96c749516da23891e7ae5
Chetan Singh <chetan.singh.0602@gmail.com>
2021-03-10 17:20:35 -0500

    added navigation

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const obj = {
+  "name": "value",
+  "details": "detail value"
+};
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title="Data" onPress={() => {navigation.navigate("Details", obj)}} />
+    </View>
+  );
+}
+
+function DetailsScreen({route, navigation}) {
+  const objOne = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen {objOne.name} {objOne.details}</Text>
+    </View>
+  );
+}
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
